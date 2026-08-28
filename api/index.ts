fa35e0dd5b9332d2eef10be1008a665e1b40aa5c
Santosh Bhandari <santooshbhandari88@gmail.com>
2026-08-28 22:21:59 +0545

    ramanage

--- api/index.ts
+++ api/index.ts
@@ -6,23 +6,38 @@ const api = axios.create({
    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
    withCredentials: true,
 });
-api.interceptors.request.use(
-   function (config) {
-      console.log("request interceptor", config.url);
-      return config;
-   },
-   function (error) {
-      console.log("request interceptor error", error);
-   },
-);
+// api.interceptors.request.use(
+//    function (config) {
+//       console.log("request interceptor", config.url);
+//       return config;
+//    },
+//    function (error) {
+//       console.log("request interceptor error", error);
+//    },
+// );
 
-api.interceptors.response.use(
-   function (config) {
-      console.log("response interceptor", config.status);
-      return config;
-   },
-   function (error) {
-      console.log("response interceptor error", error);
-   },
-)
+// api.interceptors.response.use(
+//    function (config) {
+//       console.log("response interceptor", config.status);
+//       return config;
+//    },
+//    function (error) {
+//       console.log("response interceptor error", error);
+//    },
+// )
+
+api.interceptors.request.use((config) => {
+   if (typeof window !== "undefined") {
+      const token = localStorage.getItem("token");
+
+      console.log("TOKEN:", token);
+
+      if (token) {
+         config.headers.Authorization = `Bearer ${token}`;
+      }
+   }
+
+   return config;
+});
+
 export default api
