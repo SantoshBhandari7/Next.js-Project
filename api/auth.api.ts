3b36e23b8b31ad9266294bdcda39842762c9d5f3
Santosh Bhandari <santooshbhandari88@gmail.com>
2026-08-28 21:45:33 +0545

    change api

--- api/auth.api.ts
+++ api/auth.api.ts
@@ -1,13 +1,34 @@
 
 import { TLogin, TSignUp } from "../types/auth.types";
 import api from "./index"
+// export const login = async (data: TLogin) => {
+//   try {
+//     console.log("login", data)
+//     const response = await api.post("/auth/login", data);
+//     return response.data;
+//   } catch (error: any) {
+//     throw error?.response.data
+//   }
+// };
+
 export const login = async (data: TLogin) => {
   try {
-    console.log("login", data)
     const response = await api.post("/auth/login", data);
-    return response.data;
+
+    const result = response.data;
+
+    console.log("Login response:", result);
+
+    // Adjust this according to your backend response
+    const token = result?.data?.token || result?.token;
+
+    if (token) {
+      localStorage.setItem("token", token);
+    }
+
+    return result;
   } catch (error: any) {
-    throw error?.response.data
+    throw error?.response?.data;
   }
 };
 
